fa78a0c9d9065b0ead8b280594c56eedcb22bfa1
VasilevAlexandr97 <vasilev.alex.work@gmail.com>
2021-05-16 16:59:18 +0500

    change from pipenv to poetry

--- settings.py
+++ settings.py
@@ -1,18 +1,29 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-API_TOKEN = os.environ["API_TOKEN"]
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(dotenv_path=BASE_DIR)
+
+API_TOKEN = os.getenv("API_TOKEN")
 
 DATABASE = {
-    "NAME": os.environ["DATABASE_NAME"],
-    "USER": os.environ["DATABASE_USER"],
-    "PASSWORD": os.environ["DATABASE_PASSWORD"],
-    "HOST": os.environ["DATABASE_HOST"],
+    "NAME": os.getenv("DATABASE_NAME"),
+    "USER": os.getenv("DATABASE_USER"),
+    "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+    "HOST": os.getenv("DATABASE_HOST"),
 }
 
 # База данных/тип postgresql, mysql
 DATABASE_TYPE = "postgresql"
-
 DATABASE_STR = ""
 
 if DATABASE_TYPE == "postgresql":
-    DATABASE_STR = f"postgresql://{DATABASE['USER']}:{DATABASE['PASSWORD']}@{DATABASE['HOST']}/{DATABASE['NAME']}"
+    DATABASE_STR = (
+        f"postgresql://{DATABASE['USER']}:{DATABASE['PASSWORD']}"
+        f"@{DATABASE['HOST']}/{DATABASE['NAME']}"
+    )
+
+
+
